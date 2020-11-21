b14d1430252a7a16fc180a2c50da6bbd17789949
vannguyen <vannguyen599@u.boisestate.edu>
2020-11-20 19:27:46 -0700

    github page

---
+++ script.js
@@ -0,0 +1,31 @@
+VANTA.NET({
+    el: "#vanta_background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x8b1212,
+    backgroundColor: 0x2a2a40,
+    points: 15.00,
+    maxDistance: 40.00,
+    spacing: 20.00
+  })
+  VANTA.NET({
+    el: "#vanta_footer",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x8b1212,
+    backgroundColor: 0x2a2a40,
+    points: 15.00,
+    maxDistance: 40.00,
+    spacing: 20.00
+  })
+  
